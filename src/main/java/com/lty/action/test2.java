8a37f98dd50a5c6cc5a42da07d48918577ed4253
litianyi <liheyu1996@sina.com>
2019-05-08 19:22:16 +0800

    update 04

--- src/main/java/com/lty/action/test2.java
+++ src/main/java/com/lty/action/test2.java
@@ -3,6 +3,6 @@ package com.lty.action;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("123");
-        111111111111111
+        System.out.println("1111111111");
     }
 }
