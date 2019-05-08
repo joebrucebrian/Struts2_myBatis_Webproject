6bc736a98b32aa7f656f6d425fa7d6e7654450e9
litianyi <liheyu1996@sina.com>
2019-05-08 19:24:17 +0800

    update 05

--- src/main/java/com/lty/action/test2.java
+++ src/main/java/com/lty/action/test2.java
@@ -4,5 +4,6 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("1111111111");
+        System.out.println("3333333333");
     }
 }
